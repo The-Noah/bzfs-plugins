bda8f3f72954f71fe02de290d884b91de2f00478
The Noah <thenoahbz@gmail.com>
2026-03-01 14:00:21 -0600

    chore: improve static site generation

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 const config = {
   kit: {
     adapter: adapter({
-      fallback: "index.html"
+      fallback: "404.html"
     }),
     paths: {
       base: process.argv.includes("dev") ? "" : process.env.BASE_PATH
