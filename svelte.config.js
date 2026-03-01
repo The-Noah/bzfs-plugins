72942fbab1a42693b49b855d93563cfde35ffd63
The Noah <thenoahbz@gmail.com>
2026-03-01 13:29:15 -0600

    fix: prod not having base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,15 @@
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
+const dev = process.env.NODE_ENV === "development";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
+    paths: {
+      base: dev ? "" : "/bzfs-plugins"
+    },
     alias: {
       $styles: "./src/styles"
     }
