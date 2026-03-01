14b3404000fedfea58d83ddf7890fec6c973dc05
The Noah <thenoahbz@gmail.com>
2026-03-01 13:09:19 -0600

    chore: improve cards layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,15 +34,22 @@
                 rel="noopener noreferrer">{plugin.author}</a
               >
               {#if authors[plugin.author].github}
-                • <a href="https://github.com/{authors[plugin.author].github}" target="_blank" rel="noopener noreferrer"
+                · <a href="https://github.com/{authors[plugin.author].github}" target="_blank" rel="noopener noreferrer"
                   >GitHub</a
                 >
               {/if}
               {#if authors[plugin.author].bitbucket}
-                • <a
+                · <a
                   href="https://bitbucket.org/{authors[plugin.author].bitbucket}"
                   target="_blank"
                   rel="noopener noreferrer">BitBucket</a
+                >
+              {/if}
+              {#if plugin.forum}
+                · <a
+                  href="https://forums.bzflag.org/viewtopic.php?t={plugin.forum}"
+                  target="_blank"
+                  rel="noopener noreferrer">Forum Post</a
                 >
               {/if}
             </small>
@@ -65,37 +72,33 @@
       >
       on <a href="https://github.com/The-Noah/bzfs-plugins" target="_blank" rel="noopener noreferrer">GitHub</a>.
     </p>
-    <p>Note that built-in/offical plugins are not (currently) covered.</p>
+    <small>Note that built-in/offical plugins are not (currently) covered.</small>
   </div>
 </div>
 
 <style lang="scss">
   .container {
-    width: 80%;
-    flex: 1;
-
-    @include mobile {
-      width: 90%;
-    }
+    width: 100%;
+    max-width: 1200px;
+    padding: 2rem 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 4rem;
 
     & > div {
-      margin: 4rem 0;
-
-      @include mobile {
-        margin: 2rem 0;
-      }
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
     }
   }
 
   .cards {
-    width: 100%;
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
-    grid-gap: 2rem;
+    gap: 1rem;
 
     @include mobile {
       grid-template-columns: 1fr;
-      grid-gap: 1rem;
     }
 
     & > div {
@@ -103,12 +106,17 @@
       background-color: var(--color-bg-light);
       display: flex;
       flex-direction: column;
-      box-shadow: 0 2px 1rem #0002;
+      gap: 1rem;
       border-radius: $border-radius;
+      box-shadow: 0 2px 2rem #0002;
 
       p {
-        margin: 1rem 0;
+        color: var(--color-text-light);
         flex: 1;
+      }
+
+      small {
+        margin-top: 1rem;
       }
     }
   }
