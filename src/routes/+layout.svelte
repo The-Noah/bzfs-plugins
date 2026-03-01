dcd8e3998ddde627c197cac026ae59cb8851fd10
The Noah <thenoahbz@gmail.com>
2026-03-01 12:59:42 -0600

    chore: improve layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,9 +17,6 @@
   <nav>
     {#each categories as category, i (category.name)}
       <a href="#{getCategoryId(category.name)}">{category.name}</a>
-      {#if i < categories.length}
-        •
-      {/if}
     {/each}
   </nav>
 </header>
@@ -28,67 +25,69 @@
 
 <footer>
   <div>
-    <a href="https://www.bzflag.org" target="_blank" rel="noopener noreferrer">BZFlag</a> •
+    <a href="https://www.bzflag.org" target="_blank" rel="noopener noreferrer">BZFlag</a> ·
     <a href="https://github.com/The-Noah/bzfs-plugins" target="_blank" rel="noopener noreferrer">GitHub</a>
   </div>
   <div>
-    Copyright &copy; {new Date().getFullYear()} <a href="https://noahdunbar.com" target="_blank">Noah Dunbar</a>
+    &copy; {new Date().getFullYear()} <a href="https://noahdunbar.com" target="_blank">Noah Dunbar</a>
   </div>
 </footer>
 
 <style lang="scss">
+  @use "sass:math";
+
   header {
     width: 100%;
-    height: 200px;
+    padding: 2rem 1rem;
     background:
-      linear-gradient(#0008, #000d),
+      linear-gradient(#000a, #000e),
       url("cover.jpg") no-repeat center fixed;
     background-size: cover;
+    color: #fff;
     text-align: center;
     display: flex;
     flex-direction: column;
-    justify-content: center;
     align-items: center;
-    position: relative;
+    gap: 2rem;
 
     @include mobile {
       justify-content: flex-start;
     }
 
-    & > * {
-      padding: 1rem;
-    }
-
-    * {
-      color: #fff;
-      text-shadow: 0 1px 8px #0007;
-    }
-
     nav {
-      position: absolute;
-      bottom: 0;
+      display: flex;
+      flex-wrap: wrap;
+      gap: 1rem;
+
+      @include mobile {
+        gap: 0.25rem;
+      }
+
+      a {
+        padding: 0.5rem 1rem;
+        border-radius: math.div($border-radius, 2);
+        transition:
+          background-color $tt $ease,
+          backdrop-filter $tt $ease;
+
+        &:hover {
+          background-color: rgba(0, 0, 0, 0.5);
+          backdrop-filter: blur(0.5rem);
+          text-decoration: none;
+        }
+      }
     }
   }
 
   footer {
     width: 100%;
-    padding: 1rem 0;
-    background-color: #141b22;
+    padding: 2rem;
+    background-color: #000;
+    color: #999;
     font-size: 0.9rem;
     text-align: center;
-    display: flex;
-
-    @include mobile {
-      flex-direction: column;
-    }
-
-    * {
-      color: #f0f0f0;
-    }
-
-    & > div {
-      padding: 0.5rem 1rem;
-      flex: 1;
-    }
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    gap: 2rem;
   }
 </style>
